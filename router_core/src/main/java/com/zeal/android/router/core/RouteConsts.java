be71ba1218eb1898166cf75dbbcf76ddd6a29624
zhanglf <18600082663@163.com>
2020-11-08 10:08:59 +0800

    添加备注：生成的文件路径

--- router_core/src/main/java/com/zeal/android/router/core/RouteConsts.java
+++ router_core/src/main/java/com/zeal/android/router/core/RouteConsts.java
@@ -3,6 +3,7 @@ package com.zeal.android.router.core;
 
 public class RouteConsts {
 
+    //生成的文件路径： RouterDemo\app\build\generated\ap_generated_sources\debug\out\com.zeal.android.router.core.Route_app.java
     public static final String CORE_PACKAGE_NAME = RouteConsts.class.getPackage().getName();
     public static final String METHOD_NAME = "getClazz";
 }
